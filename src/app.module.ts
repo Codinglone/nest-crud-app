71de17eb25b79f655dcfaf63900fda4a0b0ed5ef
codinglone <codinglone@gmail.com>
2023-05-22 14:48:33 +0400

    Removed imports from ucrud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UcrudModule } from './ucrud/ucrud.module';
+
 
 @Module({
-  imports: [UcrudModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
