1c4acdc1febdf8d2e14a47a24b8ec66aae1fe050
codinglone <codinglone@gmail.com>
2023-05-22 14:45:36 +0400

    Removed ucrud sample module

--- src/ucrud/dto/update-ucrud.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUcrudDto } from './create-ucrud.dto';
-
-export class UpdateUcrudDto extends PartialType(CreateUcrudDto) {}
--- src/ucrud/ucrud.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UcrudService } from './ucrud.service';
-import { UcrudController } from './ucrud.controller';
-
-@Module({
-  controllers: [UcrudController],
-  providers: [UcrudService]
-})
-export class UcrudModule {}
